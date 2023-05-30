a13e5038a5cb4240c2099ccf085fd6ea1dea5941
William A. Ferguson <wilsb8@gmail.com>
2023-05-30 11:10:13 +0000

    added about and contact pages

--- router/router.js
+++ router/router.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
 module.exports = router;
